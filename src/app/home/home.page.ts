b0c672f055fc275e9c588e3793e0bbf661bd3929
haroon-ch <haroonchlearn2earn@gmail.com>
2023-01-06 03:07:09 -0800

    home page update

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -12,17 +12,7 @@ export class HomePage {
   constructor(public route:Router,private metaTagService: Meta) {}
 
   ngOnInit() {
-    this.metaTagService.addTags([
-      {
-        name: 'keywords',
-        content: 'Angular SEO Integration, Music CRUD, Angular Universal',
-      },
-      { name: 'robots', content: 'index, follow' },
-      { name: 'author', content: 'Digamber Singh' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { name: 'date', content: '2019-10-31', scheme: 'YYYY-MM-DD' },
-      { charset: 'UTF-8' },
-    ]);
+
   }
 
  
